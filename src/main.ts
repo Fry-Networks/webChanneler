0654bc06f5dcecb5a8244a812a647da5cbe28073
Simon <44496148+siim0n@users.noreply.github.com>
2024-07-17 14:21:57 +0200

    add tiles server

--- src/main.ts
+++ src/main.ts
@@ -18,35 +18,36 @@ server.use((req, res, next) => {
 
     if (['verify.fryfoundation.com', 'verify.frynetworks.com',
         'dashboard.fryfoundation.com', 'dashboard.frynetworks.com'].includes(req.hostname)) {
-       // Serve static files for these hostnames
-       // No URL modification needed, as express.static handles it
-   } else if (['byod.fryfoundation.com', 'byod.frynetworks.com'].includes(req.hostname)) {
-       req.url = '/byod' + req.url; // Add your app path
-   } else if (['weather.fryfoundation.com', 'weather.frynetworks.com'].includes(req.hostname)) {
-       req.url = '/weather' + req.url; // Add your app path
-   } else if (['registration.fryfoundation.com', 'registration.frynetworks.com'].includes(req.hostname)) {
-       req.url = '/registration' + req.url; // Add your app path
-   } else if (['admin.fryfoundation.com', 'admin.frynetworks.com'].includes(req.hostname)) {
-       req.url = '/admin' + req.url; // Add your app path
-   } else if (['air.fryfoundation.com', 'air.frynetworks.com'].includes(req.hostname)) {
-       req.url = '/air' + req.url; // Add your app path
-   } else if (['api.fryfoundation.com', 'api.frynetworks.com'].includes(req.hostname)) {
-       req.url = '/api' + req.url; // Add your app path
-   } else if (['vote.fryfoundation.com', 'vote.frynetworks.com', ].includes(req.hostname)) {
-       req.url = '/vote' + req.url; // Add your app path
-   } else if (['water.fryfoundation.com', 'water.frynetworks.com'].includes(req.hostname)) {
-       req.url = '/water' + req.url; // Add your app path
-   } else if (['energy.fryfoundation.com', 'energy.frynetworks.com'].includes(req.hostname)) {
-       req.url = '/energy' + req.url; // Add your app path
-   } else if (req.hostname === "explorer.frynetworks.com") {
-       req.url = '/explorer' + req.url;
+        // Serve static files for these hostnames
+        // No URL modification needed, as express.static handles it
+    } else if (['byod.fryfoundation.com', 'byod.frynetworks.com'].includes(req.hostname)) {
+        req.url = '/byod' + req.url; // Add your app path
+    } else if (['weather.fryfoundation.com', 'weather.frynetworks.com'].includes(req.hostname)) {
+        req.url = '/weather' + req.url; // Add your app path
+    } else if (['registration.fryfoundation.com', 'registration.frynetworks.com'].includes(req.hostname)) {
+        req.url = '/registration' + req.url; // Add your app path
+    } else if (['admin.fryfoundation.com', 'admin.frynetworks.com'].includes(req.hostname)) {
+        req.url = '/admin' + req.url; // Add your app path
+    } else if (['air.fryfoundation.com', 'air.frynetworks.com'].includes(req.hostname)) {
+        req.url = '/air' + req.url; // Add your app path
+    } else if (['api.fryfoundation.com', 'api.frynetworks.com'].includes(req.hostname)) {
+        req.url = '/api' + req.url; // Add your app path
+    } else if (['vote.fryfoundation.com', 'vote.frynetworks.com',].includes(req.hostname)) {
+        req.url = '/vote' + req.url; // Add your app path
+    } else if (['water.fryfoundation.com', 'water.frynetworks.com'].includes(req.hostname)) {
+        req.url = '/water' + req.url; // Add your app path
+    } else if (['energy.fryfoundation.com', 'energy.frynetworks.com'].includes(req.hostname)) {
+        req.url = '/energy' + req.url; // Add your app path
+    } else if (req.hostname === "explorer.frynetworks.com") {
+        req.url = '/explorer' + req.url;
+    } else if (req.hostname === "tiles.frynetworks.com") {
+        req.url = '/tiles' + req.url;
+    } else {
+        console.log(`Unknown host ${req.hostname}`);
+        res.status(403).send('Unknown host');
+        return;
+    }
 
-   } else {
-       console.log(`Unknown host ${req.hostname}`);
-       res.status(403).send('Unknown host');
-       return;
-   }
-   
     console.log(`channeled ${req.hostname} to ${req.url}`);
     next();
 });
@@ -60,7 +61,9 @@ server.use('/api', createProxyMiddleware({ target: 'http://localhost:3011', chan
 server.use('/vote', createProxyMiddleware({ target: 'http://localhost:3012', changeOrigin: true }));
 server.use('/water', createProxyMiddleware({ target: 'http://localhost:3013', changeOrigin: true }));
 server.use('/energy', createProxyMiddleware({ target: 'http://localhost:3015', changeOrigin: true }));
+server.use('/tiles', createProxyMiddleware({ target: 'http://localhost:3018', changeOrigin: true }));
 server.use('/explorer', createProxyMiddleware({ target: 'http://localhost:3019', changeOrigin: true }));
+
 
 server.listen(port, (err?: unknown) => {
     if (err) throw err;
